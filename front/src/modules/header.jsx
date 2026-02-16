ce0f36072e3de9ed38a218f1b5102bfd8bae1e60
Mateo-Ortiz007 <elsenadordelbanco34@gmail.com>
2026-02-16 15:31:21 -0600

    no se

--- front/src/modules/header.jsx
+++ front/src/modules/header.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/header.css";
 
 function Header({ cantidad, onCarrito, onSearch }) {
+  const navigate = useNavigate(); // ✅ faltaba esto
+
   const [usuario, setUsuario] = useState(() => {
     const user = localStorage.getItem("usuario");
     return user ? JSON.parse(user) : null;
   });
 
+  const [ModalDeCerrarSesion, setModalDeCerrarSesion] = useState(false);
+
   const cerrarSesion = () => {
     localStorage.removeItem("usuario");
     setUsuario(null);
+    setModalDeCerrarSesion(false);
+    navigate("/home");
   };
 
   return (
@@ -29,11 +36,21 @@ function Header({ cantidad, onCarrito, onSearch }) {
       {usuario ? (
         <>
           <span>Hola, {usuario.nombreDeUsuario}</span>
-          <span>
-            <a href="#" onClick={cerrarSesion}>
-              Cerrar Sesion
-            </a>
-          </span>
+          <button onClick={() => setModalDeCerrarSesion(true)}>
+            Cerrar Sesion
+          </button>
+
+          {ModalDeCerrarSesion && (
+            <div className="modal-overlay-close-cesion">
+              <div className="modal-close-cesion">
+                <p>¿Seguro que deseas cerrar sesión?</p>
+                <button onClick={cerrarSesion}>Sí, cerrar</button>
+                <button onClick={() => setModalDeCerrarSesion(false)}>
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          )}
         </>
       ) : (
         <>
